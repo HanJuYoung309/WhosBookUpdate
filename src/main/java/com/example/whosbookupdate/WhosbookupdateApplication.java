5c201014fca7cd6496b66c3c4fdb3d51136dc9c4
HanJuYoung309 <juyg407@naver.com>
2025-07-10 14:28:34 +0900

    first commit

---
+++ src/main/java/com/example/whosbookupdate/WhosbookupdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.whosbookupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhosbookupdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhosbookupdateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whosbookupdate/WhosbookupdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whosbookupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhosbookupdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
